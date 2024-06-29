0212d5c61706a69f08d12fb1414ecd73fdbed45f
thindil <thindil@laeran.pl>
2024-06-29 03:39:53 +0000

    refactor: added catching exceptions in the server
    FossilOrigin-Name: fe4142688e63d91c4350985a0459f1af82bbd48a3ac8f9f49540bfab49ef57c7

--- src/server.nim
+++ src/server.nim
@@ -25,20 +25,35 @@
 
 import std/[cgi, envvars, files, hashes, os, parsecfg, paths, strtabs, strutils, streams]
 
-proc main() {.tags: [ReadEnvEffect, WriteIOEffect, ReadDirEffect, ReadIOEffect,
+proc main() {.raises: [], tags: [ReadEnvEffect, WriteIOEffect, ReadDirEffect, ReadIOEffect,
     RootEffect].} =
+
+  proc answer(message: string) {.raises: [], tags: [WriteIOEffect].} =
+    try:
+      stdout.write(message & "\n")
+    except IOError:
+      discard
+
   # Test data
   when defined(debug):
-    setTestData("key", "wertr45", "hash", "somehash", "content", "can't show error")
+    try:
+      setTestData("key", "wertr45", "hash", "somehash", "content", "can't show error")
+    except OSError:
+      answer("Status: 500 Invalid test data")
+      quit QuitFailure
 
   # Read the server's configuration
   let configFile = getEnv("RAPPORT_CONFIG")
   var fileStream = configFile.newFileStream(fmRead)
   if fileStream == nil:
-    stdout.write("Status: 500 No server configuration\n")
+    answer("Status: 500 No server configuration")
     quit QuitFailure
   var config: CfgParser
-  config.open(fileStream, configFile)
+  try:
+    config.open(fileStream, configFile)
+  except IOError, Exception:
+    answer("Status: 500 Invalid config file")
+    quit QuitFailure
   var
     keys: seq[string]
     dataDir: Path
@@ -46,7 +61,7 @@ proc main() {.tags: [ReadEnvEffect, WriteIOEffect, ReadDirEffect, ReadIOEffect,
     var entry = try:
         config.next
       except ValueError, OSError, IOError:
-        stdout.write("Status: 500 Invalid config value\n")
+        answer("Status: 500 Invalid config value")
         quit QuitFailure
     case entry.kind
     of cfgEof:
@@ -58,37 +73,57 @@ proc main() {.tags: [ReadEnvEffect, WriteIOEffect, ReadDirEffect, ReadIOEffect,
       of "datadir":
         dataDir = entry.value.Path
     of cfgError:
-      stdout.write("Status: 500 " & entry.msg & "\n")
+      answer("Status: 500 " & entry.msg)
       quit QuitFailure
     else:
       discard
-  config.close
+  try:
+    config.close
+  except OSError, IOError:
+    answer("Status: 500 Invalid config file")
   if keys.len == 0 or dataDir.string.len == 0:
-    stdout.write("Status: 500 Server not configured\n")
+    answer("Status: 500 Server not configured")
     quit QuitFailure
 
   # Read the request data
-  let request = readData()
+  let request = try:
+      readData()
+    except ValueError, IOError:
+      answer("Status: 400 Invalid data sent")
+      quit QuitFailure
   for key in ["key", "hash", "content"]:
     if key notin request:
-      stdout.write("Status: 400 No " & key & " sent.\n")
+      answer("Status: 400 No " & key & " sent.")
       quit QuitFailure
 
   # Check if the same report exist
   let
-    newHash = hash(x = request["key"] & request["hash"])
+    newHash = try:
+        hash(x = request["key"] & request["hash"])
+      except KeyError:
+        answer("Status: 500 Invalid key")
+        quit QuitFailure
     reportFile = dataDir.string & DirSep & $newHash & ".txt"
   if fileExists(reportFile.Path):
-    stdout.write("Status: 208 Report exists\n")
+    answer("Status: 208 Report exists")
     quit QuitSuccess
 
   # Create a new report file and save it in the data directory
-  let report = reportFile.open(mode = fmWrite)
-  report.writeLine("KEY: " & request["key"])
-  report.writeLine(request["content"])
-  report.close
+  let report = try:
+      reportFile.open(mode = fmWrite)
+    except IOError:
+      answer("Status: 500 Can't create report")
+      quit QuitFailure
+  try:
+    report.writeLine("KEY: " & request["key"])
+    report.writeLine(request["content"])
+  except KeyError, IOError:
+    answer("Status: 500 Invalid key")
+    quit QuitFailure
+  finally:
+    report.close
 
-  stdout.write("Status: 201 Created\n")
+  answer("Status: 201 Created")
 
 when isMainModule:
   main()
